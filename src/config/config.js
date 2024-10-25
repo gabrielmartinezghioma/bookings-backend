0c53fa8aeb9819d17193363b663722eff16d09fc
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-25 00:31:09 -0300

    advancs

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,36 @@
-require('pg')
-require('pg-hstore')
+require('pg');
+require('pg-hstore');
 require('dotenv').config();
-
 
 module.exports = {
   "development": {
     use_env_variable: "POSTGRES_DATABASE",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   "test": {
     use_env_variable: "POSTGRES_DATABASE",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   "production": {
     use_env_variable: "POSTGRES_DATABASE",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-}
+};
