6f61f81b58d12be2253d140d285604afd1f37c95
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-25 02:22:18 -0300

    advances

--- src/config/config.js
+++ src/config/config.js
@@ -2,16 +2,15 @@ require('pg')
 require('pg-hstore')
 require('dotenv').config();
 
-
 module.exports = {
   "development": {
-    use_env_variable: "POSTGRES_DATABASE",
+    use_env_variable: "DATABASE_URL_DEVELOPMENT",
   },
   "test": {
-    use_env_variable: "POSTGRES_DATABASE",
+    use_env_variable: "DATABASE_URL_TEST",
   },
   "production": {
-    use_env_variable: "POSTGRES_DATABASE_PRODUCTION",
+    use_env_variable: "DATABASE_URL_PRODUCTION",
     dialectOptions: {
       ssl: {
         require: true,
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -3,42 +3,9 @@ require('pg-hstore')
 
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const { exec } = require('child_process');
 
+const databaseUrl = process.env[`DATABASE_URL_${process.env.NODE_ENV.toUpperCase()}`] || process.env.DATABASE_URL_PRODUCTION;
 
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE_PRODUCTION, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
-
-async function runMigrations() {
-  try {
-    await sequelize.authenticate();
-    console.log('Conexión a la base de datos establecida con éxito.');
-
-    exec('npx sequelize-cli db:migrate --env production', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error al ejecutar migraciones: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Error: ${stderr}`);
-        return;
-      }
-      console.log(`Migraciones ejecutadas:\n${stdout}`);
-    });
-  } catch (error) {
-    console.error('No se pudo conectar a la base de datos:', error);
-  } finally {
-    await sequelize.close();
-  }
-}
-
-runMigrations();
+const sequelize = new Sequelize(databaseUrl)
 
 module.exports = sequelize;
